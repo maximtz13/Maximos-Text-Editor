591908eb29236a4839d9bce1670ed8a6679a325f
Maximoo <maximtz99@gmail.com>
2022-11-23 22:13:56 -0600

    small change to database.js layout

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// accepts some content and adds it to the database
 export const putDb = async (text) => {
   console.log('Post to the database');
   const contactDb = await openDB('text', 1);
@@ -23,7 +23,7 @@ export const putDb = async (text) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// gets all the content from the database
 export const getDb = async () => {
   console.log('Get from the database');
   const contactDb = await openDB('text', 1);
